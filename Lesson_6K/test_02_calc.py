da4d966c742937c3e3e07e2b1ce33ceaef3adc64
anega88 <Anega88@gmail.com>
2024-09-25 17:41:10 +0200

    test_tasks_6K

--- Lesson_6K/test_02_calc.py
+++ Lesson_6K/test_02_calc.py
@@ -4,51 +4,41 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install())
+
+def test_calculator():
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install())
     )
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-    )
-waiter = WebDriverWait(driver, 50)
+    try:
+        driver.get(
+            "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+            )
+        waiter = WebDriverWait(driver, 50)
 
-clear_field = driver.find_element(By.CSS_SELECTOR, '#delay').clear()
-sleep(2)
-kye = driver.find_element(By.CSS_SELECTOR, '#delay').send_keys('45')
+        driver.find_element(By.CSS_SELECTOR, '#delay').clear()
+        driver.find_element(By.CSS_SELECTOR, '#delay').send_keys('45')
 
-seven = driver.find_element(
-    By.CSS_SELECTOR, 'span[class="btn btn-outline-primary"]'
-    )
-seven.click()
-sleep(1)
+        driver.find_element(
+            By.CSS_SELECTOR, 'span[class="btn btn-outline-primary"]'
+            ).click()
+        driver.find_element(
+            By.CSS_SELECTOR, 'span[class="operator btn btn-outline-success"]'
+            ).click()
+        driver.find_element(
+            By.XPATH, '(//span[@class="btn btn-outline-primary"])[2]'
+            ).click()
+        driver.find_element(
+            By.CSS_SELECTOR, 'span[class="btn btn-outline-warning"]'
+            ).click()
 
-plus = driver.find_element(
-    By.CSS_SELECTOR, 'span[class="operator btn btn-outline-success"]'
-    )
-plus.click()
-sleep(1)
+        elem = waiter.until(EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, 'div[class="screen"]'), '15')
+        )
 
-eight = driver.find_element(
-    By.XPATH, '(//span[@class="btn btn-outline-primary"])[2]'
-    )
-eight.click()
-sleep(1)
+        assert elem, "Expected result '15' not found."
+        print('Correct result.')
 
-equal = driver.find_element(
-    By.CSS_SELECTOR, 'span[class="btn btn-outline-warning"]'
-    )
-equal.click()
-sleep(1)
-
-elem = waiter.until(EC.text_to_be_present_in_element(
-    (By.CSS_SELECTOR, 'div[class="screen"]'), '15')
-    )
-
-assert elem, "Expected result '15' not found."
-
-print('Correct result.')
-
-driver.quit()
+    finally:
+        driver.quit()
--- Lesson_6K/test_03_shop.py
+++ Lesson_6K/test_03_shop.py
@@ -2,65 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from time import sleep
 
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install())
+
+def test_sauce_demo_checkout():
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install())
     )
 
-driver.get("https://www.saucedemo.com/")
-driver.implicitly_wait(20)
+    try:
+        driver.get("https://www.saucedemo.com/")
+        driver.implicitly_wait(20)
 
-username = driver.find_element(By.CSS_SELECTOR, '#user-name')
-username.send_keys('standard_user')
+        username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+        username.send_keys('standard_user')
 
-password = driver.find_element(By.CSS_SELECTOR, '#password')
-password.send_keys('secret_sauce')
-sleep(1)
+        password = driver.find_element(
+            By.CSS_SELECTOR, '#password'
+            )
+        password.send_keys('secret_sauce')
 
-login = driver.find_element(
-    By.CSS_SELECTOR, '#login-button').click()
-sleep(1)
-
-take_1 = driver.find_element(
-    By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack'
-    ).click()
-sleep(1)
-
-take_2 = driver.find_element(
-    By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt'
-    ).click()
-sleep(1)
-
-take_3 = driver.find_element(
-    By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie'
-    ).click()
-sleep(1)
-
-shopping_cart = driver.find_element(
-    By.CSS_SELECTOR, 'a[class="shopping_cart_link"]'
-    ).click()
-sleep(1)
-
-checkout = driver.find_element(By.CSS_SELECTOR, '#checkout').click()
-sleep(1)
-
-first_name = driver.find_element(By.CSS_SELECTOR, '#first-name')
-first_name.send_keys('Ekaterina')
-
-last_name = driver.find_element(By.CSS_SELECTOR, '#last-name')
-last_name.send_keys('Kolesnikova')
-
-postal_code = driver.find_element(By.CSS_SELECTOR, '#postal-code')
-postal_code.send_keys('053600')
-
-button_continue = driver.find_element(By.CSS_SELECTOR, '#continue').click()
-sleep(3)
-
-total = driver.find_element(
-    By.CSS_SELECTOR, 'div[class="summary_total_label"]'
-    )
-
-print(total.text)
-
-driver.quit()
+        driver.find_element(By.CSS_SELECTOR, '#login-button').click()
+        driver.find_element(
+            By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack'
+            ).click()
+        driver.find_element(
+            By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt'
+            ).click()
+        driver.find_element(
+            By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie'
+            ).click()
+        driver.find_element(
+            By.CSS_SELECTOR, 'a[class="shopping_cart_link"]'
+            ).click()
+        driver.find_element(By.CSS_SELECTOR, '#checkout').click()
+        driver.find_element(
+            By.CSS_SELECTOR, '#first-name').send_keys('Ekaterina')
+        driver.find_element(
+            By.CSS_SELECTOR, '#last-name'
+            ).send_keys('Kolesnikova')
+        driver.find_element(
+            By.CSS_SELECTOR, '#postal-code'
+            ).send_keys('053600')
+        driver.find_element(By.CSS_SELECTOR, '#continue').click()
+        total = driver.find_element(
+            By.CSS_SELECTOR, 'div[class="summary_total_label"]'
+            )
+        print(total.text)
+        assert "Total: $" in total.text, "Total amount text not found"
+    finally:
+        driver.quit()
